01867dbdb9943bb2438170eae2cbae526109042a
walmarqui <walmarqui@gmail.com>
2021-06-16 00:14:59 -0600

    agregando entity framework Code First

--- TuDestinoHND/TuDestinoHND.BL/ProductoBL.cs
+++ TuDestinoHND/TuDestinoHND.BL/ProductoBL.cs
@@ -8,28 +8,19 @@ namespace TuDestinoHND.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> listadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            listadeProductos = new List<Producto>();
+        }
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Habitacion Familiar";
-            producto1.Precio = 1500;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Habitacion Doble";
-            producto2.Precio = 1200;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Habitacion Cencilla";
-            producto3.Precio = 900;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            listadeProductos = _contexto.Productos.ToList();
             return listadeProductos;
+
         }
     }
 }
--- TuDestinoHND/TuDestinoHND.Win/Form1.cs
+++ TuDestinoHND/TuDestinoHND.Win/Form1.cs
@@ -16,17 +16,11 @@ namespace TuDestinoHND.Win
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
             var productosBL = new ProductosBL();
             var listadeProductos = productosBL.ObtenerProductos();
 
-            foreach (var producto in listadeProductos)
-            {
-                MessageBox.Show(producto.Descripcion);
-            }
+            listadeProductosBindingSource.DataSource = listadeProductos;
         }
+
     }
 }
