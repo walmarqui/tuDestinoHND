2ff043037d0f0ad96275e8f36d412294433325de
Samuel Suazo <alejandro.mendez@unah.hn>
2021-07-28 22:41:50 -0600

    agregando cambios

--- TuDestinoHND/TuDestinoHND.Web/Controllers/HomeController.cs
+++ TuDestinoHND/TuDestinoHND.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TuDestinoHND.BL;
 
 namespace TuDestinoHND.Web.Controllers
 {
@@ -11,7 +13,12 @@ namespace TuDestinoHND.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(listadeProductos);
         }
     }
 }
