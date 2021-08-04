d1daf81de4938306bb8ed666b783a715d208005f
walmarqui <walmarqui@gmail.com>
2021-08-03 22:43:22 -0600

    agregando clase seguridad y usuario de inicio

---
+++ TuDestinoHND/TuDestinoHND.BL/SeguridadBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuDestinoHND.BL
+{
+    public class SeguridadBL
+    {
+        Contexto _contexto;
+
+        public SeguridadBL()
+        {
+            _contexto = new Contexto();
+        }
+
+        public bool Autorizar(string nombreUsuario, string contrasena)
+        {
+            var usuario = _contexto.Usuarios.FirstOrDefault(r => r.Nombre == nombreUsuario && r.Contrasena == contrasena);
+
+            if (usuario != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
